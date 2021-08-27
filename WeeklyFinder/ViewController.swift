cbcc4a67ffb5113069467fc5a4148760ed5f8add
Sergey Lobanov <stratopedarx.lobanov@yandex.ru>
2021-08-27 19:30:30 +0200

    Initial Commit

---
+++ WeeklyFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeeklyFinder
+//
+//  Created by user199993 on 8/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
